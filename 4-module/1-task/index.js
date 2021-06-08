215a2cb71b84150e5416d6ec622c85a9c82ab6a6
Anastasia <anastasiaelhova@gmail.com>
2021-06-08 23:21:10 +0300

    Правки после ревью

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,9 +1,10 @@
 function makeFriendsList(friends) {
     let listDom = document.createElement("ul");
-    friends.map((friend) => {
+
+    for(let friend of friends) {
         let li = document.createElement("li");
         li.append(`${friend.firstName} ${friend.lastName}`);
         listDom.append(li);
-    });
+    }
     return listDom;
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -2,48 +2,43 @@ let selectorCarousel = '.carousel__inner';
 let selectorCarouselSlide = '.carousel__slide';
 let selectorArrowRight = '.carousel__arrow_right';
 let selectorArrowLeft = '.carousel__arrow_left';
+let translateCurrent = 0;  // текущий сдвиг по Х. изначально - 0
 
 function initCarousel() {
   let arrowLeft = document.querySelector(selectorArrowLeft);
-  let carousel = document.querySelector(selectorCarousel);
-  carousel.dataset.translateCurrent = 0;  // текущий сдвиг по Х. изначально - 0
   arrowLeft.style.display = 'none';
-  initArrow('left');
-  initArrow('right');
+  initArrows();
 }
 
-function initArrow(direction) {
+function initArrows() {
   let carousel = document.querySelector(selectorCarousel);
   let carouselSlidesCount = carousel.querySelectorAll(selectorCarouselSlide).length;
   let arrowLeft = document.querySelector(selectorArrowLeft);
   let arrowRight = document.querySelector(selectorArrowRight);
   let frameWidth = carousel.offsetWidth;
   let translateMax = 0 - frameWidth * (carouselSlidesCount-1);
-  let arrow = document.querySelector(selectorArrowLeft);
-  if (direction === 'right') {
-    arrow = document.querySelector(selectorArrowRight);
-  }
   
-  arrow.onclick = function() {
-    let markDirection = 1;
-    if (direction === 'right') {
-      markDirection = -1;
-    }
-    let translateX = Number(carousel.dataset.translateCurrent) + markDirection * frameWidth;
+  arrowLeft.onclick = function() {
+    let translateX = translateCurrent + frameWidth;
     carousel.style.transform = `translateX(${translateX}px)`;
-    carousel.dataset.translateCurrent = translateX;
+    translateCurrent = translateX;
 
     if (translateX === 0) {
       arrowLeft.style.display = 'none';
     } else {
-      arrowLeft.style.display = '';
+      arrowRight.style.display = '';
     }
+  };
+
+  arrowRight.onclick = function() {
+    let translateX = translateCurrent - frameWidth;
+    carousel.style.transform = `translateX(${translateX}px)`;
+    translateCurrent = translateX;
 
     if (translateX === translateMax) {
       arrowRight.style.display = 'none';
     } else {
-      arrowRight.style.display = '';
+      arrowLeft.style.display = '';
     }
-
   };
 }
