055600fb612de49ae002ce2dee390c79b61d3a05
Anastasia <anastasiaelhova@gmail.com>
2021-06-11 00:13:11 +0300

    Решение 6-1 и 6-2

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,47 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
 export default class UserTable {
   constructor(rows) {
+    this.elem = document.createElement('table');
+    this.render(rows);
+    this.bindEvents();
   }
+
+  render(rows) {
+
+    let rowsInTable = rows.map((row) => `
+      <tr>
+        <td>${row.name}</td>
+        <td>${row.age}</td>
+        <td>${row.salary}</td>
+        <td>${row.city}</td>
+        <td><button>X</button></td>
+      </tr>
+    `).join('');
+
+    this.elem.innerHTML = `
+      <thead>
+        <tr>
+          <th>Имя</th>
+          <th>Возраст</th>
+          <th>Зарплата</th>
+          <th>Город</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        ${rowsInTable}
+      </tbody>
+    `;
+  }
+
+  bindEvents() {
+    let buttons = this.elem.getElementsByTagName('button');
+
+    for(let button of buttons) {
+      button.onclick = function() {
+        let row = button.parentNode.parentNode;
+        row.remove();
+      }
+    }
+  }
+
 }
